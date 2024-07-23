da1279d2cf1041701160cc17ff1a3b21c7bab628
Alexis <alexis.thomas.ines@gmail.com>
2024-07-23 09:28:37 +0200

    fix

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+	rules: {
+		'prettier/prettier': true,
+		'declaration-block-single-line-max-declarations': null // Désactive la règle
+	}
+}
