5cf356da3275d2c8a66616600187e86f23ac6981
Alexis <alexis.thomas.ines@gmail.com>
2024-07-23 08:57:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -39,8 +39,9 @@ export default defineConfig({
 	],
 	server: {
 		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
+			/* 'Cross-Origin-Opener-Policy': 'same-origin',
 			'Cross-Origin-Embedder-Policy': 'require-corp'
+		 */
 		}
 	}
 })
